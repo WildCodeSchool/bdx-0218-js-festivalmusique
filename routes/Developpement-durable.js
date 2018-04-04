abd92fb714204b33db0d01e73ec28cac2475179d
AmyLeeB <amelie_barboteau@yahoo.fr>
2018-04-04 16:30:12 +0200

    Dev Durable + Routes

---
+++ routes/Developpement-durable.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('blockcontent/Developpement-durable');
+});
+
+module.exports = router;
