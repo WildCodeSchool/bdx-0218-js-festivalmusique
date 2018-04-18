335e38b69b95baa6aedcf5beea800bf547fc4419
ValentinBz <valentin.bouzin24@gmail.com>
2018-04-18 11:38:38 +0200

    indent app.js

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ var MentionsLegales = require('./routes/mentionsLegales');
 var CGV = require('./routes/cgv');
 var Admin = require('./routes/admin');
 
-
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
