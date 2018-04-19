ab8dcdd5d58d1f2de7afe34e918f6c805403b3bf
ValentinBz <valentin.bouzin24@gmail.com>
2018-04-19 08:50:30 +0200

    resolution conflit3

--- routes/programmation.js
+++ routes/programmation.js
@@ -3,44 +3,52 @@ let router = express.Router();
 let mysql = require('mysql');
 
 let con = mysql.createConnection({
- 		host: "sql7.freesqldatabase.com",
- 		user: "sql7233133",
- 		password: "r3AcfGXI7U",
- 		database: "sql7233133"
+	host: "sql7.freesqldatabase.com",
+	user: "sql7233133",
+	password: "r3AcfGXI7U",
+	database: "sql7233133"
 });
 
 let select = 'SELECT * from artistes';
 
+//page principale
+
 router.get('/', (req, res, next) => {
-  	con.query(select, function (err, rows) {
-        if (err) throw err;
-        res.render('blockcontent/programmation', {tableArtistes: rows});
-    });
+	con.query(select, function (err, rows) {
+    if (err) throw err;
+    res.render('blockcontent/programmation', {tableArtistes: rows});
+  });
 })
 
+//tri par jour
+
 router.get('/apij', (req, res, next) => {
-    let list1 = `SELECT * from artistes`;
-    let list2 = `SELECT * from artistes where jour = '${req.query.jour}';`;
-    if (req.query.jour === "") {
-        con.query(list1, function (err, rows) {
-            if (err) throw err;
-            res.render('blockcontent/_listday', {tableArtistes: rows});
-        });
-    } else {
-        con.query(list2, function (err, rows) {
-            if (err) throw err;
-            res.render('blockcontent/_listday', {tableArtistes: rows});
-        });
-    }
+  let list1 = `SELECT * from artistes`;
+  let list2 = `SELECT * from artistes where jour = '${req.query.jour}';`;
+  if (req.query.jour === "") {
+    con.query(list1, function (err, rows) {
+      if (err) throw err;
+      res.render('blockcontent/_listday', {tableArtistes: rows});
+    });
+  } else {
+    con.query(list2, function (err, rows) {
+      if (err) throw err;
+      res.render('blockcontent/_listday', {tableArtistes: rows});
+    });
+  }
 })
+
+//tri par style
 
 router.get('/apis', (req, res, next) => {
   let listStyle = `SELECT * from artistes where style = '${req.query.style}';`;
     con.query(listStyle, function (err, rows) {
-        if (err) throw err;
-        res.render('blockcontent/_listday', {tableArtistes: rows});
+      if (err) throw err;
+      res.render('blockcontent/_listday', {tableArtistes: rows});
     });
 })
+
+//page artiste
 
 router.get('/:id(\\d+)', (req, res, next) => {
   let select = `SELECT * from artistes where kartiste = '${req.params.id}'`;
