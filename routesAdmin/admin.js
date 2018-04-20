ea6dad413c379513af75fa4d1ca188ec81b89cff
ValentinBz <valentin.bouzin24@gmail.com>
2018-04-20 09:34:21 +0200

    modif admin.js

--- routesAdmin/admin.js
+++ routesAdmin/admin.js
@@ -2,8 +2,6 @@ let express = require('express');
 let router = express.Router();
 let path = require("path");
 let mysql = require('mysql');
-let sessLog;
-let toto = "";
 
 let con = mysql.createConnection({
  		host: "sql7.freesqldatabase.com",
@@ -18,13 +16,6 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../views/blockcontentAdmin', 'adminLogin.html'));
 });
 
-coucou = (arg) => {
-	toto = arg;
-	console.log("toto in coucou : ", toto);
-}
-bonjour = () => {
-	console.log("toto in bonjour : ", toto);
-}
 router.post('/', (req, res, next) => {
 	con.query(adminID, function (err, result) {
     if (err) throw err;
