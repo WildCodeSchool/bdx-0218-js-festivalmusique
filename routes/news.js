fcb50823e0b7dc823384641c7795ad58c6493939
ValentinBz <valentin.bouzin24@gmail.com>
2018-04-25 14:08:58 +0200

    newsletter done

---
+++ routes/news.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+	host: "sql7.freesqldatabase.com",
+	user: "sql7233133",
+	password: "r3AcfGXI7U",
+	database: "sql7233133"
+});
+
+router.post('/apim', function(req, res) {
+	  let mail = `insert into subscribers (mail) values ('${req.query.mail}');`;
+	  console.log("fonction node / mail : ", req.query.mail);
+    con.query(mail, function (err, rows) {
+      if (err) throw err;
+      res.send('');
+    });
+});
+
+module.exports = router;
