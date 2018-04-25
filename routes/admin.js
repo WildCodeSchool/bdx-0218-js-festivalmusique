dc6d55a7d31db0aee0eb022b6b2f5f2f22880d83
Cyril B <33129779+cyrilBlondeau@users.noreply.github.com>
2018-04-25 11:40:20 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -45,17 +45,8 @@ router.get('/api/artiste/:id', function(req, res, next) {
 	let selectArtiste = `SELECT * from artistes where kartiste = '${req.params.id}';`;
 	con.query(selectArtiste, function (err, row) {
         if (err) throw err;
-        console.log(row);
         res.render('includesAdmin/_formArtiste', {artiste: row[0]});
     });
 });
-
-/*router.get('/abonnes', function(req, res) {
-  let selectAbonnes = `SELECT mail FROM subscribers`;
-  con.query(selectAbonnes, function(req, res) {
-    if (err) throw err;
-    res.render('blockcontentAdmin/adminAbonnes', );
-  });
-});*/
 
 module.exports = router;
