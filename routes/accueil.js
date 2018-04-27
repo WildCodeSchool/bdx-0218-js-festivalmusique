50d08286ba60ad102e771fada07bafb29b055d74
ludwig-vaan <vantoursludwig@gmail.com>
2018-04-27 10:44:40 +0200

    POUR VAL

--- routes/accueil.js
+++ routes/accueil.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+let mysql = require('mysql');
+
+// CONNECTION MYSQL
+let connection = mysql.createConnection({
+	host: "sql7.freesqldatabase.com",
+	user: "sql7233133",
+	password: "r3AcfGXI7U",
+	database: "sql7233133"
+});
+
+// IMPORT JSON FILE 
 
 /* GET home page. */
 router.get('/', function(req, res) {
 	delete req.session.login;
-  res.render('blockcontent/accueil');
+	// REQ SQL HOMEPAGE
+	let sqlRequest = `SELECT * from homepage;`;
+	connection.query(sqlRequest, function (err, results) {
+		if (err) throw err;
+		// festivalBanner is am objet with title, slogan, date props
+		res.render('blockcontent/accueil', { festivalBanner: results[0] });
+	});
 });
 
 module.exports = router;
--- routesAdmin/admin.js
+++ routesAdmin/admin.js
@@ -3,6 +3,9 @@ let router = express.Router();
 let path = require("path");
 let mysql = require('mysql');
 
+// IMPORT JSON FILE
+let homePageData = require('../data/homePageData');
+
 // UPLOAD DE FICHIER
 const multer = require('multer');
 const upload = multer({ dest: 'tmp/' });
@@ -201,6 +204,30 @@ router.post('/api/caroussel', upload.array('imageCaroussel', 3), function (req, 
 		}
 	}, 300);
 })
+
+// Traitement modification Banner
+router.post('/api/homePage/banner', function (req, res, next) {
+	console.log(req.body);
+	console.log(req.body.bannerTitle);
+	
+	const requestSQL = `UPDATE homepage SET ( title : '${req.body.bannerTitle}', slogan: '${req.body.bannerSlogan}', date: '${req.body.bannerDate}' );`
+	console.log(requestSQL);
+	
+	// con.query(deleteArtiste, function (err, row) {
+	// 	if (err) throw err;
+	// 	res.render('includesAdmin/_formArtiste');
+	// });
+
+	// res.send("")
+	res.render('blockcontentAdmin/adminFeedback', { alertType: `alert-success`, status: `La modification a été pris en compte` })
+
+	// const id = req.params.id;
+	// let deleteArtiste = `DELETE FROM artistes WHERE kartiste = '${id}';`
+	// con.query(deleteArtiste, function (err, row) {
+	// 	if (err) throw err;
+	// 	res.render('includesAdmin/_formArtiste');
+	// });
+});
 
 
 module.exports = router;
